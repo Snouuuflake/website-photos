6b7db8929d5b69cf64558f8c59915b88b595508c
Snouuuflake <xuehuamusic2@gmail.com>
2024-08-16 13:50:43 -0600

    fixed some stuff

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import sys
 import cv2
 
 from dirs import *
-from shoot import *
+from shoot import shoot
 
 # ---- making gui ----
 
 # initializing gui
+
+
 def init_gui():
     os.system("cls")
 
@@ -18,12 +20,11 @@ def menu():
     in_str = ""
     valid_op = False
 
-
-    print("Menú de Inicio:\n", "1. Nuevo Directorio\n", "2. Cargar Directorio\n", "3. Salir\n", "4. changeCamIndex\n")
-
+    print("Menú de Inicio:\n", "1. Nuevo Directorio\n",
+          "2. Cargar Directorio\n", "3. Salir\n", "4. changeCamIndex\n")
 
     while not valid_op:
-        
+
         in_str = input("Opción: ")
 
         if in_str.isdigit():
@@ -31,21 +32,19 @@ def menu():
         else:
             op = -1
 
-
         if op > 0 and op < 5:
             valid_op = True
 
         else:
             print(f"{in_str} no es una opción válida.")
 
-
-    return op        
+    return op
 
 
 # swaps contents of camIndex.txt between 0 and 1
 def changeCamIndex(cI):
     f = open("camIndex.txt", "w")
-    
+
     if cI == 0:
         f.write("1")
     elif cI == 1:
@@ -56,12 +55,13 @@ def changeCamIndex(cI):
     f.close()
 
 # returns camIndex.txt's
+
+
 def getCamIndex():
     f = open("camIndex.txt", "r")
     res = int(f.read(1))
     f.close()
     return res
-
 
 
 # --------------- main ---------------
@@ -87,6 +87,3 @@ elif option == 4:
     changeCamIndex(camIndex)
 else:
     print("how did this happen?")
-
-
-
--- shoot.py
+++ shoot.py
@@ -1,8 +1,8 @@
 import os
+
 import cv2
 
 from dirs import *
-
 
 
 def shoot(path, camNum):
@@ -13,8 +13,8 @@ def shoot(path, camNum):
 
     cam = cv2.VideoCapture(camNum)
 
-    width = 1920
-    height = 1080
+    width = 2000
+    height = 2000
     cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
     cam.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
@@ -28,28 +28,29 @@ def shoot(path, camNum):
         if not ret:
             print("!! failed to grab frame")
             break
-        cv2.imshow("Window 1", cv2.resize(frame, (0,0), fx=0.5, fy=0.5))
+        cv2.imshow("Window 1", cv2.resize(frame, (0, 0), fx=0.5, fy=0.5))
 
         k = cv2.waitKey(1)
-        if k%256 == 27:
+        if k % 256 == 27:
             # ESC pressed
             print("Cerrando")
             return 0
 
-        elif k%256 == 32:
+        elif k % 256 == 32:
             # SPACE pressed
-            
-            if input("Escriba g para guardar. \n") == 'g':
+
+            if input("Escriba g para guardar. \n").strip() == 'g':
 
                 in_name = input("Nombre del archivo (referencia): ")
-                img_name = f"{in_name}.png"  
-                img_path = os.path.join(path,img_name)
-                #print("!!!!", img_path)
+                img_name = f"{in_name}.png"
+                img_path = os.path.join(path, img_name)
+                # print("!!!!", img_path)
                 if img_name in get_files(path):
                     in_char = '-'
                     while in_char == '-':
                         in_char = '-'
-                        in_char = input(f"Archivo {in_name} ya existe. Desea reemplazarlo? s/n: ")
+                        in_char = input(
+                            f"Archivo {in_name} ya existe. Desea reemplazarlo? s/n: ")
 
                         if in_char == 's':
                             cv2.imwrite(img_path, frame)
@@ -66,7 +67,7 @@ def shoot(path, camNum):
             else:
                 print("Imagen no se guardó.")
 
-
     cam.release()
+
 
 cv2.destroyAllWindows()
